82f32c3cba7f3392e295bd22c492b3e236f79bbc
unknown <kieran6roberts@gmail.com>
2020-12-05 16:23:03 +0100

    added my-tracks page using spotify data

--- pages/dashboard/my-tracks.js
+++ pages/dashboard/my-tracks.js
@@ -1,35 +1,61 @@
 import { parseCookies } from "nookies";
 import getConfig from "next/config";
+import Image from "next/image";
 import useSWR from "swr";
 
 import Layout from "src/containers/Layout/Layout";
-import Albums from "src/components/Albums/Albums";
+import Album from "src/components/Album/Album";
 import { fetcher } from "src/hooks/useFetch";
-
+import Playlists from "src/components/Playlists/Playlists";
 
 const { publicRuntimeConfig } = getConfig();
 
 const userSavedAlbumsQuery = `/v1/me/albums?limit=10`;
 
-const myTracks = ({ savedPlaylists, userSavedAlbumsQuery }) => {
+const myTracks = ({ savedPlaylists, savedAlbums }) => {
     const { data, error } = useSWR(`${publicRuntimeConfig.SPOTIFY_API}${userSavedAlbumsQuery}`, fetcher, {
-        initialData: userSavedAlbumsQuery
+        initialData: savedAlbums
     });
 
     if (error) console.error(error);
     if (!data) console.log("loading data");
-
-    console.log(data);
-    console.log(savedPlaylists);
+    
+    const { items: albumArr } = data;
 
     return (
     <Layout>
         <main>
             <section>
+                <div className="flex items-center">
+                    <p className="capitalize mr-2">
+                    tracks from
+                    </p>
+                    <Image 
+                    src="/images/spotify-seeklogo.com.svg"
+                    alt="spotify logo"
+                    height={70}
+                    width={70} />
+                </div>
                 <h2 className="text-lg uppercase text-txt mt-8 mb-4">
-                    my tracks
+                    my playlist
                 </h2>
-               
+                 <Playlists spotifyPlaylists={savedPlaylists.items} />
+                <h2 className="text-lg uppercase text-txt mt-8 mb-4">
+                    my albums
+                </h2>
+                <ul className="grid gap-x-2 gap-y-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+                {albumArr && albumArr.map(album => 
+                    <li 
+                    className="flex justify-center items-center"
+                    key={album.album.id}>
+                        <Album
+                        name={album.album.name}
+                        artist={album.album.artists[0].name}
+                        image={album.album.images[0].url}
+                        id={album.album.id} />
+                    </li>
+                    )}
+                </ul>
             </section>
         </main>
     </Layout>
@@ -70,7 +96,7 @@ export async function getServerSideProps(ctx) {
     return {
         props: {
             savedPlaylists: savedPlaylists,
-            userSavedAlbumsQuery: savedAlbums
+            savedAlbums: savedAlbums
         }
     }
 }
--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,7 +1,10 @@
 import Album from "src/components/Album/Album";
 
 const Albums =  albums  => {
-    const { albums: newReleaseAlbums } = albums;
+    let newReleaseAlbums;
+    if (albums.albums) newReleaseAlbums = albums.albums;
+    else newReleaseAlbums = albums.tracks.items;
+
     return (
         <ul className="grid gap-x-2 gap-y-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {newReleaseAlbums.map(album => 
