bfd828ab8f71caf1c281300d52b4bec19803d4f4
unknown <kieran6roberts@gmail.com>
2020-12-05 17:01:01 +0100

    added basic styling to sidebar

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-gray-100 mt-16 p-8">
+    <footer className="border-t-2 border-gray-100 mt-16 px-8 pb-20 pt-8">
       <p className="text-md text-txt font-bold text-center">
         upRoar Music App
       </p>
+      <div className="flex justify-center items-center">
+        <p className="mr-2 capitalize">
+          powered by 
+        </p>
+          <Image 
+          src="/images/spotify-seeklogo.com.svg"
+          alt="spotify logo"
+          height={70}
+          width={70} />
+      </div>
       <p className="text-xs text-txt text-center">
         created and designed by Kieran Roberts
       </p>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Link from "next/link";
+import { GiCompactDisc } from "react-icons/gi";
+import { CgProfile, CgPlayTrackNextR } from "react-icons/cg";
+import { BiHomeAlt } from "react-icons/bi";
 import { useAuth } from "src/context/AuthContext";
 
 const Sidebar = ({ active }) => {
@@ -7,42 +10,62 @@ const Sidebar = ({ active }) => {
   return(
     <nav role="navigation">
       <div className={`fixed text-txt top-4 left-4 right-4 bottom-32 rounded z-10 bg-pri border border-gray-400
-      ${!active ? "invisible transition duration-200 ease-in opacity-0 transform scale-75" : " visible transition duration-200 ease-in opacity-1 transform scale-100"}
+      ${!active ? "invisible transition duration-200 ease-in opacity-0 transform scale-75" : "visible transition duration-200 ease-in opacity-1 transform scale-100"}
       `}>
         <h2 className="text-lg text-txt text-center py-2">
           upRoar
         </h2>
-        <div className="h-0.5 w-4/5 bg-gray-100 my-1 mx-auto"/>
-        <p className="text-sm text-center py-2">
+        <div className="h-0.5 w-4/5 bg-gray-300 my-1 mx-auto"/>
+        <p className="text-md text-center py-2">
           the place to get lost in the music
         </p>
         <div className="px-8 md:px-32 py-2">
-          <p className="ml-2 mb-4 uppercase">
+          <p className="ml-2 mb-4 text-lg text-gray-400 uppercase">
             {authUser ? "dashboard" : "new users"}
           </p>
-          <ul className="flex">
-            <li>
-              <Link href={authUser ? "/dashboard/discover" : "/login"} passHref>
-                <a className="py-1 px-2">
-                {authUser ? "discover" : "login"}
+          <ul className="flex text-sm text-center">
+            <li className="flex-auto mx-1 md:mx-4">
+              <Link href={authUser ? "/dashboard" : "/login"} passHref>
+                <a className="flex items-center capitalize py-1 px-4 rounded hover:bg-gray-100 hover:text-pri">
+                <BiHomeAlt className="mr-2 text-purple-400"/>
+                {authUser ? "home" : "login"}
                 </a>
               </Link>
             </li>
-            <li>
-              <Link href={authUser ? `/dashboard/users/${authUser}` : "/register"} passHref>
-                <a className="py-1 px-2">
-                {authUser ? "profile" : "sign up"}
+            <li className="flex-auto mx-1 md:mx-4">
+              <Link href={authUser ? `/dashboard/discover` : "/register"} passHref>
+                <a className="flex items-center capitalize py-1 px-2 rounded hover:bg-gray-100 hover:text-pri">
+                  <GiCompactDisc className="mr-2 text-yellow-400" />
+                {authUser ? "discover" : "sign up"}
                 </a>
               </Link>
             </li>
-            <li>
+            <li className="flex-auto mx-1 md:mx-4">
               <Link href={authUser ? `/dashboard/my-tracks` : ""} passHref>
-                <a className="py-1 px-2">
+                <a className="flex items-center capitalize py-1 px-2 rounded hover:bg-gray-100 hover:text-pri">
+                  <CgPlayTrackNextR className="mr-2 text-pink-400" />
                 {authUser ? "my tracks" : null}
                 </a>
               </Link>
             </li>
           </ul>
+          {authUser && 
+          <>
+          <p className="ml-2 mb-4 text-lg mt-4 text-gray-400 uppercase">
+              your account
+          </p>
+          <ul className="flex text-sm text-center">
+              <li className="mx-1 md:mx-4">
+                <Link href={`/dashboard/users/${authUser}`} passHref>
+                  <a className="flex items-center capitalize py-1 px-2 rounded hover:bg-gray-100 hover:text-pri">
+                    <CgProfile className="mr-2 text-yellow-400" />
+                    profile
+                  </a>
+                </Link>
+              </li>
+          </ul>
+          </>
+          }
         </div>
       </div>
     </nav>
