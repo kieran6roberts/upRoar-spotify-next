5f9c5c3769babb0f6afe4d4c5008af717fc9f394
unknown <kieran6roberts@gmail.com>
2020-12-24 11:34:48 +0100

    vercel env update

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   plugins: {
-    "tailwindcss": {},
     "postcss-preset-env": {
       autoprefixer: {
         flexbox: "no-2009"
       },
       stage: 3
     }
-  }
+  },
+  "tailwindcss": {}
 };
