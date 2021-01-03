8f34652f938c8f941147d1036fcc332993994b35
unknown <kieran6roberts@gmail.com>
2021-01-03 20:54:01 +0100

    vercel env fix, adjusted load fetch component

--- src/components/LoadingFetch/LoadingFetch.js
+++ src/components/LoadingFetch/LoadingFetch.js
@@ -1,7 +1,7 @@
 export default function LoadingFetch () {
     return (
         <div className="fixed top-0 left-0 flex items-center justify-center w-full h-screen bg-black bg-opacity-40 z-90">
-            <div className="flex flex-col items-center justify-center w-1/5 border rounded h-1/5 bg-pri">
+            <div className="flex flex-col items-center justify-center w-4/5 max-w-4xl border rounded h-1/5 max-h-80 bg-pri">
                 <div>
                     <span className="inline-block w-2 h-2 mx-2 rounded-full bg-txt animate-bounceFirst" />
                     <span className="inline-block w-2 h-2 mx-2 rounded-full bg-txt animate-bounceSecond" />
